433e31910cf2b7b322b20af92b491a7a362151d8
Carol Pinheiro <caroline@bossabox.com>
2024-03-19 18:50:53 -0300

    fix: add sqs as lambda event

--- src/receiver.ts
+++ src/receiver.ts
@@ -1,30 +1,33 @@
-import { RegistroDePonto } from "./interfaces/RegistroDePonto";
+import { SQSEvent } from "aws-lambda";
 import { EmailUseCase } from "./usecases/emailUseCase";
 import { assertArgumentIsValidEmail } from "@/utils/assertionConcern";
 
-const sendEmail = async (event: { email: string; data: RegistroDePonto[] }) => {
-  try {
-    const { email, data } = event;
-    const emailUseCase = new EmailUseCase();
+const sendEmail = async (event: SQSEvent) => {
+  for (const record of event.Records) {
+    try {
+      const { email, data } = JSON.parse(record.body || "{}");
 
-    const isValidEmail = assertArgumentIsValidEmail(email);
+      const emailUseCase = new EmailUseCase();
 
-    if (!isValidEmail) {
-      console.error(
-        "Skipping message: Incorrect e-mail format, ex: abx@xyz.com"
-      );
-      throw new Error("Incorrect e-mail format, ex: email@email.com");
+      const isValidEmail = assertArgumentIsValidEmail(email);
+
+      if (!isValidEmail) {
+        console.error(
+          "Skipping message: Incorrect e-mail format, ex: abx@xyz.com"
+        );
+        throw new Error("Incorrect e-mail format, ex: email@email.com");
+      }
+
+      if (!data || data.length === 0) {
+        console.error("Skipping message: No data to process");
+        throw new Error("No data to process");
+      }
+
+      await emailUseCase.sendEmail(email, data);
+    } catch (error) {
+      console.error("Error processing messages:", error);
+      throw error;
     }
-
-    if (!data || data.length === 0) {
-      console.error("Skipping message: No data to process");
-      throw new Error("No data to process");
-    }
-
-    await emailUseCase.sendEmail(email, data);
-  } catch (error) {
-    console.error("Error processing messages:", error);
-    throw error;
   }
 };
 
